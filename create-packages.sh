2c88d95567f831b72c9cbe60046f6bdc615aa4c9
Maciej Warszawski <maciej@warszawski.pro>
2018-02-08 01:53:32 +0100

    add travis config

---
+++ create-packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+yarn build && \
+yarn tsc -p tsconfig.react-intl-namespaces.json && \
+yarn tsc -p tsconfig.react-intl-namespaces-locize-client.json && \
+yarn tsc -p tsconfig.react-intl-namespaces-locize-editor.json && \
+rm -r lib/**/types.js && \
+cp -u packages/react-intl-namespaces/package.json lib/react-intl-namespaces/package.json && \
+cp -u packages/react-intl-namespaces-locize-client/package.json lib/react-intl-namespaces-locize-client/package.json && \
+cp -u packages/react-intl-namespaces-locize-editor/package.json lib/react-intl-namespaces-locize-editor/package.json && \
+echo success
