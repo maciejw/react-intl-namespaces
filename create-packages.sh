090493ad40fd58a6e70778f469fafd6db1a7c4c6
Maciej Warszawski <maciej@warszawski.pro>
2018-02-18 11:03:02 +0100

    fixup! add rest of test coverage, change es module interop to true, change logger interface, add jdoc

--- create-packages.sh
+++ create-packages.sh
@@ -3,9 +3,9 @@ yarn tsc -p tsconfig.react-intl-namespaces.json && \
 yarn tsc -p tsconfig.react-intl-namespaces-locize-client.json && \
 yarn tsc -p tsconfig.react-intl-namespaces-locize-editor.json && \
 rm -r packages/**/dist/types.js && \
-cp -u dists/react-intl-namespaces/dist/index.d.ts packages/react-intl-namespaces/dist/ && \
-cp -u dists/react-intl-namespaces-locize-client/dist/index.d.ts packages/react-intl-namespaces-locize-client/dist/ && \
-cp -u dists/react-intl-namespaces-locize-editor/dist/index.d.ts packages/react-intl-namespaces-locize-editor/dist/ && \
+cp -u dists/react-intl-namespaces/dist/index.d.ts packages/react-intl-namespaces/dist && \
+cp -u dists/react-intl-namespaces-locize-client/dist/index.d.ts packages/react-intl-namespaces-locize-client/dist && \
+cp -u dists/react-intl-namespaces-locize-editor/dist/index.d.ts packages/react-intl-namespaces-locize-editor/dist && \
 cp -u README.md packages/react-intl-namespaces && \
 cp -u README.md packages/react-intl-namespaces-locize-client && \
 cp -u README.md packages/react-intl-namespaces-locize-editor && \
