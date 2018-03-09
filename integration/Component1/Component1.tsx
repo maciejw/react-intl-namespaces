ec2aab8d18b14dc366f0222f21803e3df216e8b4
Maciej Warszawski <maciej@warszawski.pro>
2018-03-09 02:38:06 +0100

    extract messages from jsx to object

--- integration/Component1/Component1.tsx
+++ integration/Component1/Component1.tsx
@@ -1,19 +1,27 @@
 import * as React from 'react';
 import { InjectedIntlProps } from 'react-intl';
-import { FormattedMessage } from 'react-intl-namespaces';
+import { defineMessages, FormattedMessage } from 'react-intl-namespaces';
+
+const Component1Messages = defineMessages({
+  'Component1.Message': {
+    defaultMessage: 'Message from component 1',
+    id: 'Component1.Message',
+  },
+  'Component1.Message2': {
+    defaultMessage: "Message 2 from component 1",
+    id: "Component1.Message2",
+  },
+
+});
 
 export class Component1 extends React.Component<InjectedIntlProps> {
   public render() {
     return (
       <div>
-        {this.props.intl.formatMessage({
-          defaultMessage: 'Message from Component1',
-          id: 'Component1.Message',
-        })}
-
+        {this.props.intl.formatMessage(Component1Messages["Component1.Message"])}
+        {' '}
         <FormattedMessage
-          id="Component1.Message2"
-          defaultMessage="Message2 from Component1"
+          {...Component1Messages["Component1.Message2"]}
         />
       </div>
     );
--- integration/index.tsx
+++ integration/index.tsx
@@ -5,6 +5,7 @@ import * as pl from 'react-intl/locale-data/pl';
 import { Component1 } from './Component1';
 
 import {
+  defineMessages,
   FormattedMessage,
   IntlBackendProvider,
   IntlNamespaceProvider,
@@ -21,9 +22,10 @@ addLocaleData(pl[0]);
 setLogLevel(LogLevel.debug);
 
 const options: Editor.RequiredProps = {
-  apiKey: '84574b87-00ad-4c93-b493-9b6906c71c45',
+
+  apiKey: "34d03b06-b4a6-4223-850f-36f01a8e991b",
   language: 'en',
-  projectId: '049d7805-abf8-428a-82e0-0b4758bf3cce',
+  projectId: "ecbd0496-b7c7-4177-b14a-4833344b58f3",
   referenceLanguage: 'en',
 };
 const resourceServer = new LocizeClient(window, options);
@@ -103,246 +105,255 @@ namespace App {
   }
 }
 
+const AppNamespace = defineMessages({
+  'button-ok': {
+    defaultMessage: "Confirm",
+    description: "Confirmation button",
+    id: "button-ok",
+  },
+  'main-section': {
+    defaultMessage: 'App main section',
+    id: 'main-section',
+  },
+});
+
+const Module1Namespace = defineMessages({
+  'card-count': {
+    defaultMessage: "Card count: {count, number}",
+    id: "card-count",
+  },
+  'interest-rate': {
+    defaultMessage: "Interest rate: {interestRate, number, percent}",
+    id: "interest-rate",
+  },
+  'main-section': {
+    defaultMessage: "Module1 main section",
+    id: "main-section",
+  },
+
+});
+
+const Module2Namespace = defineMessages({
+  'basket-pluralization': {
+    defaultMessage: `You have {itemCount, plural,
+      =0 {no items}
+      one {1 item}
+      other {{itemCount} items}
+  }.`,
+    id: "basket-pluralization",
+  },
+  'last-visited-date-medium': {
+    defaultMessage: "Page was visited {date, date, medium}",
+    id: "last-visited-date-medium",
+  },
+  'last-visited-long': {
+    defaultMessage: "Page was visited {date, time, long}",
+    id: "last-visited-long",
+
+  },
+  'last-visited-relative': {
+    defaultMessage: "Page was visited {date}",
+    id: "last-visited-relative",
+  },
+  'last-visited-short': {
+    defaultMessage: "Page was visited {date, time, short}",
+    id: "last-visited-short",
+  },
+
+  'last-visited-time-medium': {
+    defaultMessage: "Page was visited {date, time, medium}",
+    id: "last-visited-time-medium",
+  },
+
+  'messages-count': {
+    defaultMessage: `
+    Hello {gender, select,
+      male {Mr.}
+      female {Mrs.}
+      other {}
+      } {name} you have unread {count, plural,
+        =0{no messages}
+        one {# message}
+        other {# messages}
+        }`,
+    id: "messages-count",
+  },
+  'response-gender': {
+    defaultMessage: `{gender, select,
+      male {He}
+      female {She}
+      other {They}
+  } will respond shortly.`,
+    id: "response-gender",
+  },
+  'tax-info': {
+    defaultMessage: `{taxableArea, select,
+      yes {An additional {taxRate, number, percent} tax will be collected.}
+      other {No taxes apply.}
+  }`,
+    id: "tax-info",
+  },
+});
+
 ReactDom.render(
   <App languages={['en', 'pl']}>
     <IntlNamespaceProvider namespace="App">
-      <div style={{ margin: '20px', pointerEvents: 'none' }}>
-        <Component1 />
-        <FormattedMessage id="main-section" defaultMessage="App main section" />
-        <div>
-          <button>
-            <FormattedMessage
-              id="button-ok"
-              defaultMessage="Confirm"
-              description="Confirmation button"
-            />
-          </button>
-        </div>
-      </div>
-    </IntlNamespaceProvider>
-    <IntlNamespaceProvider namespace="Module1">
       <div>
-        <FormattedMessage
-          id="main-section"
-          defaultMessage="Module1 main section"
-        />
-        <div>
-          <FormattedMessage
-            id="card-count"
-            defaultMessage="Card count: {count, number}"
-            values={{ count: 6 }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="interest-rate"
-            defaultMessage="Interest rate: {interestRate, number, percent}"
-            values={{ interestRate: 0.12 }}
-          />
-        </div>
-      </div>
-    </IntlNamespaceProvider>
-    <IntlNamespaceProvider namespace="Module2" includeNamespace={['App']}>
-      <div>
-        <div>
-          <FormattedMessage
-            id="last-visited-short"
-            defaultMessage="Page was visited {date, time, short}"
-            values={{ date: new Date().setHours(-26) }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="last-visited-date-medium"
-            defaultMessage="Page was visited {date, date, medium}"
-            values={{ date: new Date().setHours(-26) }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="last-visited-time-medium"
-            defaultMessage="Page was visited {date, time, medium}"
-            values={{ date: new Date().setHours(-26) }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="last-visited-long"
-            defaultMessage="Page was visited {date, time, long}"
-            values={{ date: new Date().setHours(-26) }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="last-visited-relative"
-            defaultMessage="Page was visited {date}"
-            values={{
-              date: (
-                <FormattedRelative
-                  units="hour"
-                  value={new Date().setHours(-26)}
-                />
-              ),
-            }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="response-gender"
-            defaultMessage={`{gender, select,
-              male {He}
-              female {She}
-              other {They}
-          } will respond shortly.`}
-            values={{ gender: 'female' }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="response-gender"
-            defaultMessage={`{gender, select,
-              male {He}
-              female {She}
-              other {They}
-          } will respond shortly.`}
-            values={{ gender: 'male' }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="tax-info"
-            defaultMessage={`{taxableArea, select,
-              yes {An additional {taxRate, number, percent} tax will be collected.}
-              other {No taxes apply.}
-          }`}
-            values={{ taxableArea: 'yes', taxRate: 0.03 }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="tax-info"
-            defaultMessage={`{taxableArea, select,
-              yes {An additional {taxRate, number, percent} tax will be collected.}
-              other {No taxes apply.}
-          }`}
-            values={{ taxableArea: 'no' }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="basket-pluralization"
-            defaultMessage={`You have {itemCount, plural,
-              =0 {no items}
-              one {1 item}
-              other {{itemCount} items}
-          }.`}
-            values={{ itemCount: 0 }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="basket-pluralization"
-            defaultMessage={`You have {itemCount, plural,
-              =0 {no items}
-              one {1 item}
-              other {{itemCount} items}
-          }.`}
-            values={{ itemCount: 1 }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="basket-pluralization"
-            defaultMessage={`You have {itemCount, plural,
-              =0 {no items}
-              one {1 item}
-              other {{itemCount} items}
-          }.`}
-            values={{ itemCount: 2 }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="basket-pluralization"
-            defaultMessage={`You have {itemCount, plural,
-              =0 {no items}
-              one {1 item}
-              other {{itemCount} items}
-          }.`}
-            values={{ itemCount: 3 }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="basket-pluralization"
-            defaultMessage={`You have {itemCount, plural,
-              =0 {no items}
-              one {1 item}
-              other {{itemCount} items}
-          }.`}
-            values={{ itemCount: 4 }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="basket-pluralization"
-            defaultMessage={`You have {itemCount, plural,
-              =0 {no items}
-              one {1 item}
-              other {{itemCount} items}
-          }.`}
-            values={{ itemCount: 8 }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="basket-pluralization"
-            defaultMessage={`You have {itemCount, plural,
-              =0 {no items}
-              one {1 item}
-              other {{itemCount} items}
-          }.`}
-            values={{ itemCount: 22 }}
-          />
-        </div>
-        <div>
-          <FormattedMessage
-            id="messages-count"
-            defaultMessage={`
-            Hello {gender, select,
-              male {Mr.}
-              female {Mrs.}
-              other {}
-              } {name} you have unread {count, plural,
-                =0{no messages}
-                one {# message}
-                other {# messages}
-                }`}
-            values={{ count: 0, name: 'John', gender: 'male' }}
-          />
+        <div style={{ margin: '20px', pointerEvents: 'none' }}>
+          <Component1 />
+          <FormattedMessage {...AppNamespace["main-section"]} />
+          <div>
+            <button>
+              <FormattedMessage {...AppNamespace["button-ok"]}
+              />
+            </button>
+          </div>
         </div>
 
-        <div>
-          <FormattedMessage
-            id="messages-count"
-            defaultMessage={`
-            Hello {gender, select,
-              male {Mr.}
-              female {Mrs.}
-              other {}
-              } {name} you have {count, plural,
-                =0{no unread messages}
-                one {# unread message}
-                other {# unread messages}
-                }`}
-            values={{ count: 8, name: 'Jane', gender: 'female' }}
-          />
-        </div>
-        <div>
-          <button>
-            <FormattedMessage id="App:button-ok" defaultMessage="Confirm" />
-          </button>
-        </div>
+        <IntlNamespaceProvider namespace="Module1">
+          <div>
+            <FormattedMessage
+              {...Module1Namespace["main-section"]}
+            />
+            <div>
+              <FormattedMessage
+                {...Module1Namespace["card-count"]}
+                values={{ count: 6 }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module1Namespace["interest-rate"]}
+                values={{ interestRate: 0.12 }}
+              />
+            </div>
+          </div>
+        </IntlNamespaceProvider>
+        <IntlNamespaceProvider namespace="Module2" includeNamespace={['App']}>
+          <div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["last-visited-short"]}
+                values={{ date: new Date().setHours(-26) }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["last-visited-date-medium"]}
+                values={{ date: new Date().setHours(-26) }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["last-visited-time-medium"]}
+                values={{ date: new Date().setHours(-26) }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["last-visited-long"]}
+                values={{ date: new Date().setHours(-26) }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["last-visited-relative"]}
+                values={{
+                  date: (
+                    <FormattedRelative
+                      units="hour"
+                      value={new Date().setHours(-26)}
+                    />
+                  ),
+                }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["response-gender"]}
+                values={{ gender: 'female' }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["response-gender"]}
+                values={{ gender: 'male' }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["tax-info"]}
+                values={{ taxableArea: 'yes', taxRate: 0.03 }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["tax-info"]}
+                values={{ taxableArea: 'no' }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["basket-pluralization"]}
+                values={{ itemCount: 0 }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["basket-pluralization"]}
+                values={{ itemCount: 1 }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["basket-pluralization"]}
+                values={{ itemCount: 2 }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["basket-pluralization"]}
+                values={{ itemCount: 3 }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["basket-pluralization"]}
+                values={{ itemCount: 4 }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["basket-pluralization"]}
+                values={{ itemCount: 8 }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["basket-pluralization"]}
+                values={{ itemCount: 22 }}
+              />
+            </div>
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["messages-count"]}
+                values={{ count: 0, name: 'John', gender: 'male' }}
+              />
+            </div>
+
+            <div>
+              <FormattedMessage
+                {...Module2Namespace["messages-count"]}
+                values={{ count: 8, name: 'Jane', gender: 'female' }}
+              />
+            </div>
+            <div>
+              <button>
+                <FormattedMessage id="App:button-ok" defaultMessage="Confirm" />
+              </button>
+            </div>
+          </div>
+        </IntlNamespaceProvider>
       </div>
     </IntlNamespaceProvider>
   </App>,
